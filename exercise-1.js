7b4a0a449603a8df9f24810514e229933f4f36d0
Alessandro Ricci <alessandroricci@MBPdiAlessandro.homenet.telecomitalia.it>
2022-08-19 18:20:53 +0200

    js-49

---
+++ exercise-1.js
@@ -0,0 +1,15 @@
+class Person {
+  constructor(id, firstName, lastName, age) {
+    this.id = id;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+  }
+}
+
+const developer = new Person(1, 'Mario', 'Rossi', 25);
+
+const Personjson = JSON.stringify(developer);
+
+console.log(Personjson);
+// Print developer as json object
